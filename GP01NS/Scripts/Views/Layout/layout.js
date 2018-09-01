655a97008994e30a8da192ca749e94accd514ec0
Richard Silva <talk@richardds.com.br>
2018-09-01 05:57:16 -0300

    formulario de cadastro de músico

---
+++ GP01NS/Scripts/Views/Layout/layout.js
@@ -0,0 +1,46 @@
+﻿function fieldsRequest() {
+	$('[required]').each(function () {
+		const textError = $(this).attr('placeholder') || 'Preencha esse campo';
+		$(this).after(`<div class="invalid-feedback">${textError}!</div>`)
+
+	});
+}
+
+function selectCustom() {
+	$('.select-custom').each((idx, el) => {
+		const placeholder = $(el).attr('placeholder') || '';
+
+		$(el).select2({
+			placeholder
+		});
+	});
+}
+
+
+function textareaAutoresize() {
+	$('textarea').each(function() {
+		const
+			it = this,
+			heigthInit = $(it).height(),
+			heigthEnd = 500;
+
+		$(it)
+			.on('change cut paste drop keydown', function() {				
+				setTimeout(() => {
+					$(it).height('auto');
+					const scrollHeight = $(it).prop('scrollHeight');
+					if (scrollHeight > heigthInit && scrollHeight < heigthEnd) {
+						$(it).height(scrollHeight);
+					} else {
+						$(it).height(heigthEnd);
+					}
+				}, 0);
+			});
+	});	
+}
+
+$(function () {
+	fieldsRequest()
+	selectCustom();
+	textareaAutoresize();
+});
