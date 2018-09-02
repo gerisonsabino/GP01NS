af5f4cc9e390fa5a25a46e1cd7563e3ebcc9fbc8
lucaslima-7 <lucas.lima.7@outlook.com>
2018-09-02 20:01:41 -0300

    Removi bastante coisa que não precisava dos dois plugins e criei um só arquivo .js

---
+++ GP01NS/Scripts/Views/Entrar/Cadastro/form_main.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+    /* Criação das Máscaras */
+
+    //Pega a data atual do sistema, apenas a data (DD)
+    var data = new Date();
+
+    //Expressão Regular Para Validar os Campos E-mail
+    var valida_email = /[a - z0 - 9!#$ %& '*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&' * +/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+
+    $(".cpf").inputmask({ 'mask': '999.999.999-99' });
+    $(".cnpj").inputmask({ 'mask': '99.999.999/9999-99' });
+    $(".datepicker").inputmask({ alias: "datetime", inputFormat: "dd/mm/yyyy", placeholder: "__/__/____" });
+    $(".datepicker").datepicker({ language: "pt-BR", format: "dd/mm/yyyy", autoclose: true, endDate: data.getDate().toString() });
+    $(".cep").inputmask({ 'mask': '99999-999' });
+    $(".email").attr("pattern", valida_email);
+    $(".celular").inputmask({ 'mask': '(99)9999-9999[9]' });
+
+    /*Validação dos Campos*/
+    $("form").attr("novalidate", " ");
+    $("form").addClass("needs-validation");
+    //Coloca na variável tudo aquilo que precisar de validação
+    $(".needs-validation").on('submit', function (event) {
+        $(".email").focusout(function () {
+            if (valida_email.test($(".email").val()) == true) {
+                $(".email").removeClass("form-control:invalid").addClass("form-control:valid");
+            }
+        });
+        //Limpa o campo Confirme a Senha se os valores forem diferentes
+        if ($(".senha").val() != $(".confirmaSenha").val()) {
+            $(".confirmaSenha").val('');
+        }
+        //Faz a validação de tudo o que está com required, enquanto estiver, trava o submit
+        if ($(".needs-validation")[0].checkValidity() === false) {
+            $(".needs-validation").addClass("was-validated");
+            event.preventDefault();
+        }
+     });
+
+    //Checa se algum input tem a classe inválido e se tiver mostra a div de alerta
+    if ($("input").hasClass(".is-invalid")) {
+        $(".invalid-feedback").show;
+    }
+});
